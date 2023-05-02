25edb622cdfd64297f0944440f9c4b3cbf081203
12mhudon <128413833+12mhudon@users.noreply.github.com>
2023-05-02 15:01:06 -0400

    added basic home page content

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -7,7 +7,12 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    #return "Welcome Home"
+    url_params = dict(request.args)
+    print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
+
+    # compile message using specified "name" url parameter or a default value:
+    name = url_params.get("name") or "Guest"
+    message = f"Hello, {name}!"
     return render_template("home.html")
 
 @home_routes.route("/overview")
@@ -18,14 +23,5 @@ def overview():
 @home_routes.route("/history")
 def history():
     print("HISTORY...")
-
-    url_params = dict(request.args)
-    print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
-
-    # compile message using specified "name" url parameter or a default value:
-    name = url_params.get("name") or "World"
-    message = f"Hello, {name}!"
-
-    #return message
     return render_template("history.html", message=message, other='YEAH', x='5')
 
