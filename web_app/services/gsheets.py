932a6708cf67464da62b9c7b0bdae1447fa71825
mp1860 <128503775+mp1860@users.noreply.github.com>
2023-05-06 12:00:23 -0400

    fixed message boxes

--- web_app/services/gsheets.py
+++ web_app/services/gsheets.py
@@ -1,26 +1,11 @@
 import pygsheets
 import pandas as pd
+import collections as col
+
 #authorization
 def push_to_sheets(coin1,coin2):
     gc = pygsheets.authorize(service_file='ibt-proj-81e46f86a85f.json')
 
-    # # Create empty dataframe
-    
-    # df = pd.DataFrame()
-
-    # # Create a column
-    # df['coins'] = []
-    # df.append
-    
-
-    # #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-    # sh = gc.open('Favorite coins')
-
-    # #select the first sheet 
-    # wks = sh[0]
-
-    # #update the first sheet with df, starting at cell B2. 
-    # wks.set_dataframe(df,(1,1))
     sh = gc.open('Favorite coins')
     user_coins = [coin1,coin2]
     wk1 = sh[0]
@@ -34,3 +19,20 @@ def push_to_sheets(coin1,coin2):
     print(new_df)
     wk1.set_dataframe(new_df,(1,1))
     wk1.update_value('A1','coins')
+    return coin_list
+
+
+# def top_coins(input_list):
+#     if push_to_sheets:
+#         coin_counts = col.Counter(input_list)
+#         sorted_counts = sorted(coin_counts.items(),key=lambda x:x[1],reverse=True)
+#         top_coin = sorted_counts[0]
+#         top_coin_name, top_coin_count = top_coin
+#         print("TOP COIN:", top_coin_name, "COUNT:", top_coin_count)
+#         top_coin2 = sorted_counts[1]
+#         top_coin_name2, top_coin_count2 = top_coin2
+#         print("TOP COIN:", top_coin_name2, "COUNT:", top_coin_count2)
+#         top_coin3 = sorted_counts[2]
+#         top_coin_name3, top_coin_count3 = top_coin3
+#         print("TOP COIN:", top_coin_name3, "COUNT:", top_coin_count3)
+#         return top_coin_name,top_coin_count,top_coin_name2,top_coin_count2,top_coin_name3,top_coin_count3
