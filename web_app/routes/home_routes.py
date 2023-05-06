d535d1e4977c6d219190e7566c42fdaadc6c705b
mp1860 <128503775+mp1860@users.noreply.github.com>
2023-05-06 12:37:19 -0400

    QOL improvements

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -9,13 +9,7 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    url_params = dict(request.args)
-    print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
-
-    # compile message using specified "name" url parameter or a default value:
-    name = url_params.get("name") or "Guest"
-    message = f"Hello, {name}!"
-    return render_template("home.html",message=message)
+    return render_template("home.html")
 
 @home_routes.route("/overview")
 def overview():
